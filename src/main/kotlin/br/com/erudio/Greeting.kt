93c6478573dad364cf86a1db160e55925723f069
wallacehermano <wallacehermano@gmail.com>
2024-05-28 19:42:33 -0300

    62.0604

--- src/main/kotlin/br/com/erudio/Greeting.kt
+++
@@ -1,4 +0,0 @@
-package br.com.erudio
-
-class Greeting (val id: Long, val content: String) {
-}
--- src/main/kotlin/br/com/erudio/Startup.kt
+++ src/main/kotlin/br/com/erudio/Startup.kt
@@ -4,8 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Startup
+open class Startup
 
 fun main(args: Array<String>) {
 	runApplication<Startup>(*args)
+	//para testes digitar no navegador
+	//http://localhost:8080/sum/1/2
 }
